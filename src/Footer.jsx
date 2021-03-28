d9bb89866f29ee5bdccf156696dfe3bc4a9f4a14
Vivek Jha <Vivekkumar4704@gmail.com>
2021-03-28 11:00:29 +0530

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+     <>
+      <footer className="bg-light text-center">
+      
+      <p> &copy; 2021 VivekTEchnical. All Rights Reserved | Terms And Condition</p>
+
+      </footer>
+     </>
+  );
+};
+
+export default Footer;
---
+++ src/Sdata.jsx
@@ -0,0 +1,36 @@
+import web from "../src/img/img-1.jpg";
+import app from "../src/img/img-2.jpg";
+import android from "../src/img/img-3.jpg";
+import digital from "../src/img/img-4.jpg";
+import marketing from "../src/img/img-5.jpg";
+import software from "../src/img/img-6.jpg";
+
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title:"Web Development",
+  },
+  {
+    imgsrc: app,
+    title: "App Development",
+  },
+  {
+    imgsrc: software,
+    title: "Software Development",
+  },
+  {
+    imgsrc: digital,
+    title: "Digital Marketing",
+  },
+  {
+    imgsrc: android,
+    title: "Android Development",
+  },
+  {
+    imgsrc: marketing,
+    title: "Marketing",
+  },
+];
+
+export default Sdata;
